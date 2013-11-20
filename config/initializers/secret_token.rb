15d64cd3104520497e0874ce7b6339bc2c92febc
mostlyclicks <carlos@mostlyclicks.com>
2013-11-20 10:19:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salonsolo::Application.config.secret_token = '07b1d1affc987d65f2684ea586e18c67e089706dca9176ff340940565cd6eec1fd5f8fca64dd23306568a3fd28129bd72c1ebb51ef2b032f92f3f3efb6036c15'
